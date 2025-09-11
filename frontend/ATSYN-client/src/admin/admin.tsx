fe157f813d6874bcc31d8a51e84165de83524553
Carrington Wynn <carringtonwynn@gmail.com>
2025-09-11 17:19:40 -0500

    admin basic setup

---
+++ frontend/ATSYN-client/src/admin/admin.tsx
@@ -0,0 +1,11 @@
+import AdminNavigation from "./admin-navigation"
+//import AdminDashboardHeader from "./admin-dashboard-header"
+
+export default function AdminMain() {
+  return (
+    <>
+    
+    <AdminNavigation />
+    </>
+  );
+}
--- frontend/ATSYN-client/src/main.tsx
+++ frontend/ATSYN-client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+import Admin from './admin/admin.tsx'
+//import AdminNavigation from "./admin/admin-navigation"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div>
+    {/* <App /> */}
+    <Admin/>
+
+    </div>
+    
   </StrictMode>,
 )
