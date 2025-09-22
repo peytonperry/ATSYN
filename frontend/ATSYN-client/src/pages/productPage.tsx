ff97deb706ec425be9ab959bf15e3d45cff7f4e5
Keira Stewart <stewartkeira5@gmail.com>
2025-09-22 14:18:12 -0500

    more small stuff

--- frontend/ATSYN-client/src/pages/productPage.tsx
+++ frontend/ATSYN-client/src/pages/productPage.tsx
@@ -1,4 +1,12 @@
-import { Container, Grid, Image, Title, Text } from "@mantine/core";
+import {
+  Container,
+  Grid,
+  Image,
+  Title,
+  Text,
+  GridCol,
+  Select,
+} from "@mantine/core";
 import { useEffect, useState } from "react";
 
 //https://148896138.cdn6.editmysite.com/uploads/1/4/8/8/148896138/23NCB5VZT2CNCWXZ3BH4YKPU.jpeg?width=2560&optimize=medium
@@ -17,17 +25,36 @@ function ProductPage() {
       "https://148896138.cdn6.editmysite.com/uploads/1/4/8/8/148896138/23NCB5VZT2CNCWXZ3BH4YKPU.jpeg?width=2560&optimize=medium",
   };
 
+  const [quantity, setQuantity] = useState<string | null>('1');
+  const quantityOptions = Array.from({ length: Math.min(productSample.stockAmount, 10)}, (_, i) => ({
+    value: String(i + 1),
+    label: String(i + 1)
+  }));
+
+
+
+
+
   return (
     <Container>
-      <Grid>
-        <Grid.Col span={{ base: 12, md: 6 }}>
+      <Grid gutter={{base: 5, xs: 'md', md: 'xl', xl: 50}}>
+        <Grid.Col span={4}>
           <Image src={productSample.image} radius="md" />
         </Grid.Col>
 
-        <Grid.Col span={{base: 12, md: 6}}>
-          <Title order={1} mb="md">{productSample.title}</Title>
-          <Text size="xl" mb="md">{productSample.price}</Text>
-          <Text size="lg" mb="md">{productSample.description}</Text>
+        <Grid.Col span={4}>
+          <Title order={1} mb="md">
+            {productSample.title}
+          </Title>
+          <Text size="xl" mb="md">
+            {productSample.price}
+          </Text>
+          <Text size="lg" mb="md">
+            {productSample.description}
+          </Text>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Select data={quantityOptions} />
         </Grid.Col>
       </Grid>
     </Container>
