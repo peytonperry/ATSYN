29c28235e5406ebf91ca87738b272a5c9686016b
Keira Stewart <stewartkeira5@gmail.com>
2025-09-22 22:59:45 -0500

    3 column layout set up. Not pretty, need to fix more

--- frontend/ATSYN-client/src/pages/ProductDetail.tsx
+++ frontend/ATSYN-client/src/pages/ProductDetail.tsx
@@ -1,5 +1,16 @@
-import { Container, Grid, Image, Title, Text, Select } from "@mantine/core";
-import { useState } from "react";
+import {
+  Container,
+  Grid,
+  Image,
+  Title,
+  Text,
+  Select,
+  Stack,
+  Paper,
+  Radio,
+  Button,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
 import { apiService } from "../config/api";
 import { useParams } from "react-router-dom";
 
@@ -23,7 +34,7 @@ interface Product {
 }
 
 function ProductDetailPage() {
-  const [product, setProduct] = useState<Product>([]);
+  const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
@@ -33,47 +44,79 @@ function ProductDetailPage() {
       console.log(data);
       setProduct(data);
       setLoading(false);
-
-
     } catch (error) {
       setLoading(false);
       console.error("API call failed:", error);
     }
   };
 
-  /*const [quantity, setQuantity] = useState<string | null>("1");
+  const [quantity, setQuantity] = useState<string | null>("1");
 
-  const quantityOptions = product ? Array.from(
-    { length: Math.min(product.stockAmount, 10) },
-    (_, i) => ({
-      value: String(i + 1),
-      label: String(i + 1),
-    })
-  );*/
+  const quantityOptions = product
+    ? Array.from({ length: product.stockAmount }, (_, i) => ({
+        value: String(i + 1),
+        label: String(i + 1),
+      }))
+    : [];
 
-  fetchData();
+  useEffect(() => {
+    fetchData();
+  }, [id]);
 
   return (
-    <Container>
-      <Grid gutter={{ base: 5, xs: "md", md: "xl", xl: 50 }}>
-        <Grid.Col span={4}>
-          <Image src={product.imageUrl} radius="md" />
+    <Container size="xl">
+      <Grid>
+        <Grid.Col span={{ base: 12, md: 5 }}>
+          <Image src={product?.imageUrl} radius="sm" fit="contain" h={400} />
         </Grid.Col>
+        {/* Left */}
+        <Grid.Col span={{ base: 12, md: 4 }}>
+          <Stack gap="md">
+            <Title order={1}>{product?.title}</Title>
+            <Text size="md">{product?.description}</Text>
+          </Stack>
+        </Grid.Col>
+        {/* Middle*/}
+        <Grid.Col span={{ base: 12, md: 3 }}>
+          <Paper withBorder p="md" radius="md">
+            <Stack gap="md">
+              <Text size="xl" fw={700}>
+                ${product?.price}
+              </Text>
+              <Text size="sm">Free Returns</Text>
+              <Text size="sm">Free Delivery</Text>
+            </Stack>
 
-        <Grid.Col span={4}>
-          <Title order={1} mb="md">
-            {product.title}
-          </Title>
-          <Text size="xl" mb="md">
-            {product.price}
-          </Text>
-          <Text size="lg" mb="md">
-            {product.description}
-          </Text>
+            <div>
+              <Text size="sm">Quantity:</Text>
+              <Select
+                value={quantity}
+                onChange={setQuantity}
+                data={quantityOptions}
+              />
+            </div>
+
+            <div>
+              <Text size="sm" mb="xs">
+                Shipping:
+              </Text>
+              <Radio.Group value="shipping" name="ShippingOptions">
+                <Stack>
+                  <Radio label="Shipping" />
+                  <Radio label="Local delivery" />
+                  <Radio label="Store Pickup" />
+                </Stack>
+              </Radio.Group>
+            </div>
+            <Button fullWidth size="md" variant="outline">
+              Add to Cart
+            </Button>
+            <Button fullWidth size="md" variant="outline">
+              Buy Now
+            </Button>
+          </Paper>
         </Grid.Col>
-        <Grid.Col span={4}>
-          <Select />
-        </Grid.Col>
+        {/* Right */}
       </Grid>
     </Container>
   );
