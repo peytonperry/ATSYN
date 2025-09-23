9c3b3c131b1e8d32073a8b2987ca9e48215a84e0
peytonperry <65989807+peytonperry@users.noreply.github.com>
2025-09-22 19:14:45 -0500

    More homepage changes
    Also added some changes to nav bar

--- frontend/ATSYN-client/src/pages/Homepage/Homepage.tsx
+++ frontend/ATSYN-client/src/pages/Homepage/Homepage.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import "./Homepage.css";
-
+import React from 'react';
+import background from '../Homepage/ATSYN_Logo.png'
 const HomePage = () => {
   return (
     <div className="homepage-wrapper">
-      <div className="main-content">
-        <div className="bg-animations">
-          <div className="anim-shape shape-1"></div>
-          <div className="anim-shape shape-2"></div>
-          <div className="anim-shape shape-3"></div>
-          <div className="anim-shape shape-4"></div>
-          <div className="anim-shape shape-5"></div>
-          <div className="anim-shape shape-6"></div>
-        </div>
+      <div 
+        className="background-image"
+        style={{
+          backgroundImage: `url(${background})` 
+        }}
+      ></div>
+      
+      <div className="dark-overlay"></div>
 
+      <div className="main-content">
         <div className="text-content">
           <h1 className="hero-title">ALL THE SHIT YOU NEED</h1>
           <div className="tagline-text">Because shit matters.</div>
