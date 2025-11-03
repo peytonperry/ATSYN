52a5dd74c1f5069a4f6847e985454d217d8bb6c0
peytonperry <65989807+peytonperry@users.noreply.github.com>
2025-11-03 16:50:27 -0600

    starting stripe
    Got the basics on the backend filled out. moving on to the frontend but I will need to make another endpoint for stripe I think.

---
+++ ATSYN.Api/Controllers/StripeSettings.cs
@@ -0,0 +1,7 @@
+namespace ATSYN.Api.Controllers;
+
+public class StripeSettings
+{
+        public string SecretKey { get; set; } = string.Empty;
+        public string PublishableKey { get; set; } = string.Empty;
+}
--- ATSYN.AppHost/Program.cs
+++ ATSYN.AppHost/Program.cs
@@ -7,6 +7,10 @@ var sqlServerPasswprd = builder
 
 var smtp2goApiKey = builder.AddParameter("smtp2go-api-key", secret: true);
 
+var stripeSecretKey = builder.AddParameter("stripe-secret-key", secret: true);
+var stripePublishableKey = builder.AddParameter("stripe-publishable-key", secret: true);
+
+
 var sql = builder
     .AddSqlServer(name: "ATSYN-SqlServer", port: 1433, password: sqlServerPasswprd)
     .WithLifetime(ContainerLifetime.Persistent)
@@ -23,6 +27,8 @@ var api = builder.AddProject<Projects.ATSYN_Api>("api")
     .WithEnvironment("Email__ApiKey", smtp2goApiKey)
     //replace email with clients 
     .WithEnvironment("Email__SenderAddress", "brennan.kimbrell@selu.edu")
+    .WithEnvironment("Stripe__SecretKey", stripeSecretKey)
+    .WithEnvironment("Stripe__PublishableKey", stripePublishableKey)
     .WaitForCompletion(migrations)   
     .WithExternalHttpEndpoints()
     .WaitFor(db);
