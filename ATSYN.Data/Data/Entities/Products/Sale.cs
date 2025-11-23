fcc7ff55b32efa7e4b7b1e86a50ef46c59f75bf9
King-Junco <brennankimbrell0601@gmail.com>
2025-11-23 14:45:29 -0600

    created class Sale

---
+++ ATSYN.Data/Data/Entities/Products/Sale.cs
@@ -0,0 +1,27 @@
+﻿using ATSYN.Api.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATSYN.Data.Data.Entities.Products
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public decimal SalePrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
